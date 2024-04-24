96b9b29d0837dce85308518207eb570851d905d9
Jaeho Cho <jaeho2025@gmail.com>
2024-04-24 18:44:10 -0400

    animate figure

--- figure_1.m
+++ figure_1.m
@@ -19,16 +19,15 @@ z_limits = [-1.5, 0.5];
 % Create the figure and subplot for the cone
 figure;
 subplot(1, 2, 1); % First subplot in a 1x2 grid
-surf(X, Y, Z, 'FaceAlpha', 0.5) % Create surface plot with 50% transparency
-hold on; % Hold the current plot to add lines
+hCone = surf(X, Y, Z, 'FaceAlpha', 0.5); % Create surface plot with 50% transparency
+hold on; % Hold the current plot
 
-% Add a line down the middle of the cone
-line([0 0], [0 0], [0 -1], 'Color', 'red', 'LineWidth', 2) % Vertical line down the Z axis
+% Initialize the red line on the cone
+hLineCone = line([0 0], [0 0], [0 -1], 'Color', 'red', 'LineWidth', 2);
 
 % Add a line starting at the origin and pointing left
 line([0 -1], [0 0], [0 0], 'Color', 'blue', 'LineWidth', 2) % Horizontal line along the negative X axis
 
-% Set axis properties
 axis equal % Ensure all axes have the same scale
 xlim(x_limits);
 ylim(y_limits);
@@ -36,10 +35,9 @@ zlim(z_limits);
 xlabel('X-axis')
 ylabel('Y-axis')
 zlabel('Z-axis')
-title('Translucent Cone')
+title('Dependent Components')
 view(3); % Set 3D view for the cone plot
 grid on; % Turn on grid for the cone plot
-hold off; % Release the hold to finish plotting
 
 % Define vertices for a pyramid pointing downward
 V = [1 1 -1; -1 1 -1; -1 -1 -1; 1 -1 -1; 0 0 0]; % Base and apex
@@ -47,15 +45,15 @@ F = [1 2 5; 2 3 5; 3 4 5; 4 1 5; 1 2 3; 3 4 1]; % Faces connecting vertices
 
 % Subplot for the pyramid
 subplot(1, 2, 2); % Second subplot in a 1x2 grid
-patch('Vertices', V, 'Faces', F, 'FaceColor', 'blue', 'FaceAlpha', 0.5)
-hold on; % Hold the current plot to add lines
+hPyramid = patch('Vertices', V, 'Faces', F, 'FaceColor', 'blue', 'FaceAlpha', 0.5);
+hold on; % Hold the current plot
 
-% Add a line from the apex to the base center
-line([0 0], [0 0], [0 -1], 'Color', 'red', 'LineWidth', 2) % Vertical line down the Z axis from apex to base center
+% Initialize the red line on the pyramid
+hLinePyramid = line([0 0], [0 0], [0 -1], 'Color', 'red', 'LineWidth', 2);
+
 % Add a line starting at the origin and pointing left
 line([0 -1], [0 0], [0 0], 'Color', 'blue', 'LineWidth', 2) % Horizontal line along the negative X axis
 
-% Set the same axis properties
 axis equal % Ensure all axes have the same scale
 xlim(x_limits);
 ylim(y_limits);
@@ -63,7 +61,21 @@ zlim(z_limits);
 xlabel('X-axis')
 ylabel('Y-axis')
 zlabel('Z-axis')
-title('Translucent Pyramid')
+title('Independent Components')
 view(3); % Set 3D view for the pyramid plot
 grid on; % Turn on grid for the pyramid plot
+
+% Endless animation loop for the rotating line
+while ishandle(hCone) % Continue as long as the figure exists
+    for t = linspace(0, 2*pi, 360)
+        % Update line position in the cone plot
+        set(hLineCone, 'XData', [0 cos(t)], 'YData', [0 sin(t)], 'ZData', [0 -1]);
+
+        % Update line position in the pyramid plot
+        set(hLinePyramid, 'XData', [0 cos(t)], 'YData', [0 sin(t)], 'ZData', [0 -1]);
+
+        drawnow % Update the plots
+    end
+end
+
 hold off; % Release the hold to finish plotting
